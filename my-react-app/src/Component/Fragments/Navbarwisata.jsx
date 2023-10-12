9fccbb49cbfc476c06fa556198281a8756c74821
RaihanJL <raihanlaban123@gmail.com>
2023-10-12 09:05:03 +0700

    wisata commit

--- my-react-app/src/Component/Fragments/Navbarwisata.jsx
+++ my-react-app/src/Component/Fragments/Navbarwisata.jsx
@@ -7,16 +7,17 @@ const Navbarwisata = () => {
 return (
     <Navbar className = "Nav">
     <Container>
-        <Navbar.Brand style={{color:'white'}} href="#home">Tour Tech</Navbar.Brand>
+        <Navbar.Brand style={{color:'white', textDecoration:'none'}} href="#home">Tour Tech</Navbar.Brand>
         <Navbar.Toggle />
         <Nav className="justify-content-end" activeKey="/home" >
             <Nav.Item>
-            <Nav.Link style={{color: 'white'}} href="/hom">Beranda</Nav.Link>
+            <Nav.Link style={{color: 'white', marginRight:'50px'}} href="/home">Beranda</Nav.Link>
             </Nav.Item>
             <NavDropdown 
                 id="nav-dropdown-dark-example"
                 title={<span style={{ color: 'white' }}>Wisata</span>}
                 menuVariant="white"
+                style={{marginRight:'50px'}}
                 >
                 <NavDropdown.Item href="#action/3.1">Alam</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
@@ -28,6 +29,7 @@ return (
                 id="nav-dropdown-dark-example"
                 title={<span style={{ color: 'white' }}>Pemandu</span>}
                 menuVariant="white"
+                style={{marginRight:'50px'}}
                 >
                 <NavDropdown.Item href="#action/3.1">Data Pemandu</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
@@ -35,12 +37,13 @@ return (
                 </NavDropdown.Item>    
             </NavDropdown>
             <Nav.Item>
-            <Nav.Link style={{color: 'white'}} eventKey="/testimoni" >Testimoni</Nav.Link>
+            <Nav.Link style={{color: 'white', marginRight:'50px'}} eventKey="/testimoni" >Testimoni</Nav.Link>
             </Nav.Item>
             <NavDropdown
                 id="nav-dropdown-dark-example"
                 title={<span style={{ color: 'white' }}>User</span>}
                 menuVariant="white"
+                
                 >
                 <NavDropdown.Item href="#action/3.1">Akun Saya</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
---
+++ my-react-app/src/pages/Wisatapages.jsx
@@ -0,0 +1,101 @@
+import Navbarwisata from "../Component/Fragments/Navbarwisata.jsx";
+import CenteredCard from "../Component/Fragments/wisatacard.jsx";
+import Bgimage from "../assets/wisata-budaya.png";
+import React from "react";
+import Bgimage2 from "../assets/wisata-budaya2.png";
+import pic1 from "../assets/tradisional1.png"
+import pic2 from "../assets/tradisional2.png"
+import pic3 from "../assets/tradisional3.png"
+import pic4 from "../assets/img/makanan/Kasuami 1.png"
+import pic5 from "../assets/img/makanan/Kue-Bagea-1 1.png"
+import pic6 from "../assets/img/makanan/Tuli-Tuli 1.png"
+
+const  Wisatapages = () =>{
+    return(
+    <>   
+        <div
+            className="bg-image img-fluid"
+            style={{
+            backgroundImage: `url(${Bgimage})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center",
+            height: "85vh",
+            fontFamily: "Times New Roman"
+            }}    
+        >
+            <Navbarwisata/>
+            <div className="label">
+                <div className="flexcontainer">
+                    <p className="text">
+                    <span className="text-wrapper">
+                        Explore Budaya
+                        <br />
+                    </span>
+                    </p>
+                    <p className="text">
+                    <span className="text-wrapper">Buton Tengah</span>
+                    </p>
+                </div>
+            </div>
+            <CenteredCard/>
+            <div className="bg-image img-fluid"
+            style={{
+            backgroundColor: "#D9D9D9",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center",
+            height: "85vh",
+            }}>
+            </div>
+            <div className="bg-image"
+            style={{
+            backgroundImage: `url(${Bgimage2})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center",
+            height: "85vh",
+            }}>
+                <div style={{paddingTop: '50px', lineHeight:'4px'}} className="text-center text-light">
+                <h1 className="mb-3">Tari Tradisional</h1>
+                <p>Tari-tarian tradisional Buton sangat menarik dan bisa menjadi bagian dari pengalaman</p>
+                <p>budaya Anda. Berbagai tarian seperti Tari Kaci, Tari Pakewa, dan Tari Binesa sering </p>
+                <p>ditampilkan dalam acara-acara khusus.</p>
+                </div>
+                <div className=" mt-5 d-flex justify-content-center align-items-center">
+                    <img className="pe-5" src={pic1}></img>
+                    <img className="pe-5" src={pic2}></img>
+                    <img src={pic3}></img>
+                </div>
+            </div>
+            <div className="bg-image"
+            style={{
+                backgroundColor: "#D9D9D9",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center",
+            height: "85vh",
+            }}>
+                <div style={{paddingTop: '4rem'}} className=" text-dark text-center">
+                    <h1 className="">Kuliner Khas Buton</h1>
+                </div>
+                <div style={{lineHeight: '1px'}} className="text-dark text-center mt-3">
+                    <p>Berada di kabupaten Buton seolah berada di surga karena meski berukuran kecil, namun pulau ini memiliki </p>
+                    <p>banyak destinasi wisata alam yang sangat menawan. Apalagi pulau Buton juga dikenal penuh dengan pilihan</p>
+                    <p>aneka kuliner yang kaya citarasa. Beberapa makanan khas daerahnya juga dianggap sebagai warisan kerajaan</p>
+                    <p>Buton, yang dulu pernah berjaya di Sulawesi Tenggara.</p>
+                </div>
+                <div style={{marginTop: '5rem'}} className="d-flex justify-content-center align-items-center">
+                    <img className=" pe-4"  src={pic4}></img>
+                    <img className="pe-4" src={pic5}></img>
+                    <img  src={pic6}></img>
+                </div>
+            </div>
+        </div>
+    </>
+        
+    )
+
+}
+
+export default Wisatapages;
